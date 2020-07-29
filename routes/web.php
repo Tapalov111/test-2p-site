a6c869a7b650aec959cb699e518859c912023476
Temirlan <temirlan2017almaty@gmail.com>
2020-07-29 14:17:54 +0600

    j

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,12 @@
 <?php
 
-Route::get('/', function () {
+Route::get('/main', function () {
     return view('main');
 });
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< HEAD
 Route::get('/',function() {
     return view('welcome');
 });
-=======
-Route::get('/boss', function () {
-    return view('boss');
-});
-
->>>>>>> b0ba4932f26c6f9640e550679faeaa053b0adf1d
-=======
-Route::get('/',function() {
-    return view('welcome');
-});
->>>>>>> Stashed changes
-=======
-Route::get('/',function() {
-    return view('welcome');
-});
->>>>>>> Stashed changes
-=======
-Route::get('/',function() {
-    return view('welcome');
-});
->>>>>>> Stashed changes
 
 Auth::routes();
 
